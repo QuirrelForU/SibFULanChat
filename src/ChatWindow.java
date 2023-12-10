77540e0858b2aa1bf93212a4617ece85b0bd185f
QuirrelForU <symmlux@gmail.com>
2023-12-11 02:19:07 +0700

    version 1.2.0.1 Save chat history method

--- src/ChatWindow.java
+++ src/ChatWindow.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ChatWindow extends JFrame implements ChatObserver {
     private JTextArea chatArea;
     private JTextField inputField;
     private JButton sendButton;
+    private JButton saveButton;
     private Client client;
 
     public ChatWindow(Client client) {
@@ -16,11 +22,19 @@ public class ChatWindow extends JFrame implements ChatObserver {
         chatArea.setEditable(false);
         inputField = new JTextField(20);
         sendButton = new JButton("Send");
+        saveButton = new JButton("Save Chat");
 
         sendButton.addActionListener(e -> {
             String message = inputField.getText();
             client.sendMessage(message);
             inputField.setText("");
+        });
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveChatToFile();
+            }
         });
 
         JPanel panel = new JPanel();
@@ -32,8 +46,12 @@ public class ChatWindow extends JFrame implements ChatObserver {
         inputPanel.add(inputField, BorderLayout.CENTER);
         inputPanel.add(sendButton, BorderLayout.EAST);
 
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.add(inputPanel, BorderLayout.CENTER);
+        bottomPanel.add(saveButton, BorderLayout.EAST);
+
         add(panel, BorderLayout.CENTER);
-        add(inputPanel, BorderLayout.SOUTH);
+        add(bottomPanel, BorderLayout.SOUTH);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 300);
@@ -43,5 +61,21 @@ public class ChatWindow extends JFrame implements ChatObserver {
     @Override
     public void update(String message) {
         chatArea.append(message + "\n");
+    }
+
+    private void saveChatToFile() {
+        try {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setDialogTitle("Specify a file to save");
+            int userSelection = fileChooser.showSaveDialog(this);
+            if (userSelection == JFileChooser.APPROVE_OPTION) {
+                String filePath = fileChooser.getSelectedFile().getAbsolutePath();
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+                    writer.write(chatArea.getText());
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
